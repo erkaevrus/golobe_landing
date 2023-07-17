7c73f2cf49f4aa79f792ea9b7cfe2877bc77ed4a
erkaevrus <erk_08@mail.ru>
2023-07-17 12:45:22 +0400

    feat: add reviews section, add footer, add subscribe-form

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ import '@/assets/sprites/swap-horizontal.svg'
 import '@/assets/sprites/arrow-drop-down.svg'
 import '@/assets/sprites/plus.svg'
 import '@/assets/sprites/paper-plane.svg'
+import '@/assets/sprites/star.svg'
+import '@/assets/sprites/google.svg'
+import '@/assets/sprites/facebook.svg'
+import '@/assets/sprites/twitter.svg'
+import '@/assets/sprites/youtube.svg'
+import '@/assets/sprites/instagram.svg'
 
 
 // ---IMG---
